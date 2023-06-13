7ae09fc50cc6cfd08b88fda570b0226f75e1ba03
Aquademon <98233916+Aquademon@users.noreply.github.com>
2023-06-13 22:19:59 +0300

    Balance.js

--- Commands/Economy/Balance.js
+++ Commands/Economy/Balance.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  type: "interaction",
+  prototype: 'slash',
+  name: "balance",
+  code: `
+  $interactionReply[;{newEmbed:
+  {author:$userTag:$userAvatar}
+  {title:Balance}
+  {description:> **💵 | You have \`$getGlobalUserVar[Money;$authorID]\` coins**}
+  {color:$getVar[EmbedColor]}
+  {footer:Requested by $userTag}
+  {timestamp}
+  };;;all;false]
+  `
+}
