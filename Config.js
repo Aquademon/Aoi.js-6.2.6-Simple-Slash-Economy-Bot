2fd7f5d2b7e1d9d1c0590e9727d27c0f1af0acde
Aquademon <98233916+Aquademon@users.noreply.github.com>
2023-06-13 22:02:56 +0300

    Config.js

---
+++ Config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Bot: {
+    Token: "TOKEN HERE",
+    Prefix: "PRRFİX HERE",
+    },
+  Status: {
+    Text: "Simple Economy Bot",
+    Type: "PLAYING",
+    Status: "idle",
+    Time: 12,
+    }
+}
